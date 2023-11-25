764d7f3fbbdc5437e677f3621a68223f4b3e1076
nurkhasan <nur.khasan.ind@gmail.com>
2023-11-25 15:37:46 +0700

    Edit HTML, Edit JS, dan Edit README.md supaya lebih asseekk

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ Belajar mengakses kamera belakang dengan HTML
 
 ## Manfaat
 Mempelajari cara mengakses kamera dan mengambil gambarnya secara detail.
+
+Dapat menyimpan gambar / upload ke server setelah ambil dari kamera.
+
+## Sumber Artikel Terkait
 Sumber: https://www.digitalocean.com/community/tutorials/front-and-rear-camera-access-with-javascripts-getusermedia-id
--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 feather.replace();
 
 const controls = document.querySelector('.controls');
-const cameraOptions = document.querySelector('.video-options>select');
 const video = document.querySelector('video');
 const canvas = document.querySelector('canvas');
-const screenshotImage = document.querySelector('#screenshot-image');
 const buttons = [...controls.querySelectorAll('button')];
 const ketcam = document.getElementById('keteranganCam');
 const tbKirim = document.querySelector('#kirim');
@@ -27,22 +25,8 @@ var constraints = {
       ideal: 1080,
       max: 1440 },
 
-    facingMode: cameraOptions.value // Menentukan depan/belakang = "user" (depan) / "environment" (belakang) ==> https://www-webdevdrops-com.translate.goog/en/how-to-access-device-cameras-with-javascript/?_x_tr_sl=en&_x_tr_tl=id&_x_tr_hl=id&_x_tr_pto=tc&_x_tr_hist=true
+    facingMode: 'environment' // Menentukan depan/belakang = "user" (depan) / "environment" (belakang) ==> https://www-webdevdrops-com.translate.goog/en/how-to-access-device-cameras-with-javascript/?_x_tr_sl=en&_x_tr_tl=id&_x_tr_hl=id&_x_tr_pto=tc&_x_tr_hist=true
        } };
-//VAR Dump Fungsi
-function vardump(obj) {
-  var out = '';
-  for (var i in obj) {
-    out += i + ": " + obj[i] + "\n";
-  }
-alert(out);
-}
-// EVEN untuk pilih Kamera.
-cameraOptions.onchange = () => {
-  constraints.video.facingMode = cameraOptions.value;
-  const updatedConstraints = constraints;
-  startStream(updatedConstraints);
-};
 
 // EVEN ketika tombol PLAY di klik
 const VidPlay = () => {
@@ -53,12 +37,11 @@ const VidPlay = () => {
     return;
   }
   if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
-    constraints.video.facingMode = cameraOptions.value;
+    constraints.video.facingMode = 'environment';
     const updatedConstraints = constraints;
     startStream(updatedConstraints);
   }
   setTimeout(ketCamera, 3000);
-  screenshotImage.classList.add('d-none');
   tbKirim.classList.add('d-none');
 };
 
@@ -72,31 +55,15 @@ const pauseStream = () => {
 // EVEN untuk rutin AMBIL Foto
 const doScreenshot = () => {
   canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  screenshotImage.style.width = video.offsetWidth+'px';
-  screenshotImage.style.height = video.offsetHeight+'px';  
+  canvas.height = video.videoHeight; 
   canvas.getContext('2d').drawImage(video, 0, 0);
-  screenshotImage.src = canvas.toDataURL('image/webp');
   signature64.value = canvas.toDataURL('image/webp');
-  screenshotImage.classList.remove('d-none');
   pauseStream();
   tbKirim.classList.remove('d-none');
 };
 
-const doKetCam = () => {
-  if (screenshotImage.classList.contains('d-none')){
-    screenshotImage.classList.remove('d-none');
-  } else {
-    screenshotImage.classList.add('d-none');
-  }
-  screenshotImage.style.width = video.offsetWidth+'px';
-  screenshotImage.style.height = video.offsetHeight+'px';  
-}
-
 play.onclick = VidPlay;
-pause.onclick = doScreenshot; //pauseStream;
-//screenshot.onclick = doScreenshot;
-ketcam.onclick = doKetCam;
+pause.onclick = doScreenshot;
 
 
 //RUTIN steam GAMBAR
@@ -117,17 +84,4 @@ const ketCamera = () => {
   ketcam.innerHTML = ketcam.innerHTML + 'L Offset = ' +video.offsetWidth+' T Offset = '+video.offsetHeight;
 }
 
-// Membuat LIST pilihan kamera ke dropdown
-/*
-const getCameraSelection = async () => {
-  const devices = await navigator.mediaDevices.enumerateDevices();
-  const videoDevices = devices.filter(device => device.kind === 'videoinput');
-  const options = videoDevices.map(videoDevice => {
-    return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
-  });
-  cameraOptions.innerHTML = options.join('');
-};
-
-getCameraSelection();
-*/
-//# sourceURL=pen.js
+VidPlay();

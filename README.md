7c1d2d5547d406cbc0c76282698e5abffd0119d2
nurkhasan <nur.khasan.ind@gmail.com>
2023-12-03 19:51:42 +0700

    Menambahkan Fungsi Upload Gambar Secara Interaktif dan KecHee

--- README.md
+++ README.md
@@ -1,10 +1,32 @@
-# Akses Kamera HTML Sederhana
+Akses Kamera HTML Sederhana
+===========================
+
 Belajar mengakses kamera belakang dengan HTML
 
-## Manfaat
-Mempelajari cara mengakses kamera dan mengambil gambarnya secara detail.
+ 
 
-Dapat menyimpan gambar / upload ke server setelah ambil dari kamera.
+Manfaat
+-------
 
-## Sumber Artikel Terkait
-Sumber: https://www.digitalocean.com/community/tutorials/front-and-rear-camera-access-with-javascripts-getusermedia-id
+-   Mempelajari cara mengakses kamera dan mengambil gambarnya secara detail
+
+    Sumber :
+    <https://www.digitalocean.com/community/tutorials/front-and-rear-camera-access-with-javascripts-getusermedia-id>
+
+-   Dapat menyimpan gambar / upload ke server setelah ambil dari kamera.
+
+    Sumber :
+    <https://www.sistemit.com/membuat-tanda-tangan-touch-screen-html-php-javascript/>
+
+-   Membuat form upload gambar interaktip dengan sedikit bumbu javaskrip yg
+    keche. Sumber :
+    [https://uploadcare.com/blog/how-to-upload-files-using-js](https://uploadcare.com/blog/how-to-upload-files-using-js/)
+
+ 
+
+Catatan
+-------
+
+Semua code ini hanya semata untuk belajar. Tidak ada lisensi dan juga tidak ada
+jaminan dapat berjalan langsung. Serta untuk resiko silahkan ditanggung sendiri.
+Belajarlah dengan tekun. Mudah-mudahan InshaAllah berhasil jadi prgrammer Keren.
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Semua KOnstanta urusan Ambil Gambar
 feather.replace();
 
 const controls = document.querySelector('.controls');
@@ -5,8 +6,6 @@ const video = document.querySelector('video');
 const canvas = document.querySelector('canvas');
 const buttons = [...controls.querySelectorAll('button')];
 const ketcam = document.getElementById('keteranganCam');
-const tbKirim = document.querySelector('#kirim');
-const signature64 =  document.querySelector('#signature64');
 
 let streamStarted = false;
 
@@ -28,6 +27,15 @@ var constraints = {
     facingMode: 'environment' // Menentukan depan/belakang = "user" (depan) / "environment" (belakang) ==> https://www-webdevdrops-com.translate.goog/en/how-to-access-device-cameras-with-javascript/?_x_tr_sl=en&_x_tr_tl=id&_x_tr_hl=id&_x_tr_pto=tc&_x_tr_hist=true
        } };
 
+// Semua KOnstanta urusan Upload Gambar
+  const form = document.querySelector('form');
+  const statusMessage = document.getElementById('statusMessage');
+  //const fileInput = document.querySelector('input');
+  const progressBar = document.querySelector('progress');
+  const tbKirim = document.querySelector('#kirim');
+  const gambar64 =  document.querySelector('#gambar64');
+
+// ############################# Semua Rutin Urusan AMBIL Gambar KAMERA
 // EVEN ketika tombol PLAY di klik
 const VidPlay = () => {
   if (streamStarted) {
@@ -57,7 +65,7 @@ const doScreenshot = () => {
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight; 
   canvas.getContext('2d').drawImage(video, 0, 0);
-  signature64.value = canvas.toDataURL('image/webp');
+  gambar64.value = canvas.toDataURL('image/jpeg', 1.0); //==>('image/webp');
   pauseStream();
   tbKirim.classList.remove('d-none');
 };
@@ -85,3 +93,51 @@ const ketCamera = () => {
 }
 
 VidPlay();
+
+// ############################# Semua Rutin Urusan Upload FORM
+            function updateStatusMessage(text) {
+                    statusMessage.innerHTML = text;
+                }
+            function showPendingState() {
+                    tbKirim.disabled = true;
+                    updateStatusMessage('-->> Menunggu ...')
+                }
+            function updateProgressBar(value) {
+                    const percent = value * 100;
+                    progressBar.value = Math.round(percent);
+                }          
+
+            tbKirim.onclick = (event) => {
+                event.preventDefault();
+                showPendingState();
+                uploadFiles();
+            }
+
+            function uploadFiles() {
+                const url = 'upload.php';
+                const method = 'post';
+
+                const xhr = new XMLHttpRequest();
+
+                const data = new FormData(form);
+
+                xhr.addEventListener('loadend', () => {
+                    if (xhr.status === 200) {
+                        updateStatusMessage('Success');
+                        updateProgressBar(0);
+                        ketcam.innerHTML = xhr.responseText;
+                        tbKirim.disabled = false;
+                    } else {
+                        updateStatusMessage('Error');
+                        tbKirim.disabled = false;
+                    }
+                });
+
+                xhr.upload.addEventListener('progress', event => {
+                    updateStatusMessage(`<br/>==> ${event.loaded} of ${event.total} byte`);
+                    updateProgressBar(event.loaded / event.total);
+                });
+
+                xhr.open(method, url);
+                xhr.send(data);
+            }
